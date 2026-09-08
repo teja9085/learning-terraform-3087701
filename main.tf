168dadd16bea5548112fd899e421bf231e8444bb
teja9085 <tejaswinikodakandla@gmail.com>
2026-09-08 11:15:24 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
