d484847fa37d46761e77e0f5360a3e6e0d177893
teja9085 <tejaswinikodakandla@gmail.com>
2026-09-08 11:04:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,10 @@
 data "aws_ami" "app_ami" {
   most_recent = true
 
+  owners = ["amazon"]
+
   filter {
     name   = "name"
-    values = ["bitnami-tomcat*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-
-  tags = {
-    Name = "HelloWorld"
+    values = ["al2023-ami-2023*"]
   }
 }
