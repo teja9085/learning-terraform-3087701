9be2c365b73aae18f2c29e26f59c79b8f9f9223f
teja9085 <tejaswinikodakandla@gmail.com>
2026-09-09 13:17:21 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,54 +1,94 @@
+#################################################
+# Latest Amazon Linux 2 AMI
+#################################################
+
 data "aws_ami" "app_ami" {
   most_recent = true
 
+  owners = ["amazon"]
+
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
-
-  filter {
-    name   = "virtualization-type"
+*  filter {
+    name   = "virtualiz*tion-type"
     values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
+  *
 }
+
+##############################*##################
+# VPC
+#########*##################################*####
 
 module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source*=*"terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+ *name = "dev-vpc"
+  cid* = "10.0.0.0/16"
 
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs = [
+    "us-west-2a",
+    "us-west-2b",
+    "us-west-2c"
+  ]
 
+  public_subnets =*[
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24"
+  ]
 
-  tags = {
-    Terraform = "true"
-    Environment = "dev-test"
+* enable_dns_hostnames = true
+  ena*le_dns_support   = true
+
+  tags = *
+    Terraform   = "true"
+    Envi*onment = "dev-test"
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
+#######*##################################*######
+# Security Group
+##########*##################################*###
 
 module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+ *source* = "terraform-aws-modules/security*group/aws"
+  version = "~> 5.0"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+ *name   = "blog-sg"
+  vpc_id = modu*e.blog_vpc.vpc_id
+
+  ingress_rules*= [
+    "http-80-tcp",
+    "https-443-tcp",
+    "ssh-tcp"
+  ]
+
+  ingr*ss_cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+
+  egress_rules = [
+    "all-all"
+  ]
+
+  egress_cidr_blocks = [
+    "0.0.0.0/0"
+  ]
+}
+
+#############*##################################*
+# EC2
+###########################*#####################
+
+resource "a*s_instance" "blog" {
+  ami        *           = data.aws_ami.app_ami.*d
+  instance_type          = var.i*stance_type
+  subnet_id           *  = module.blog_vpc.public_subnets*0]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  t*gs = {
+    Name = "LearningTerrafo*m"
+  }
 }
