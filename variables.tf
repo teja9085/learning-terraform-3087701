da813727b9db88ad8bb91ac294426e892acd5530
teja9085 <tejaswinikodakandla@gmail.com>
2026-09-09 14:09:05 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "instance_type" {
-  description = "Type of EC2 instance to provision"
-  default     = "t3.nano"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
