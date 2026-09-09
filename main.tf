54bbf183127f71c63cc1d3b29bc5aaaa55fe8d70
teja9085 <tejaswinikodakandla@gmail.com>
2026-09-09 13:54:51 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,32 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+#################################################
+# Variables
+#################################################
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
 #################################################
 # Latest Amazon Linux 2 AMI
 #################################################
 
-data "aws_ami" "app_ami" {
+data "aws_ami" "amazon_linux" {
   most_recent = true
 
   owners = ["amazon"]
@@ -11,84 +35,144 @@ data "aws_ami" "app_ami" {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
-*  filter {
-    name   = "virtualiz*tion-type"
+
+  filter {
+    name   = "virtualization-type"
     values = ["hvm"]
-  *
+  }
 }
 
-##############################*##################
+#################################################
 # VPC
-#########*##################################*####
+#################################################
 
-module "blog_vpc" {
-  source*=*"terraform-aws-modules/vpc/aws"
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
 
- *name = "dev-vpc"
-  cid* = "10.0.0.0/16"
-
-  azs = [
-    "us-west-2a",
-    "us-west-2b",
-    "us-west-2c"
-  ]
-
-  public_subnets =*[
-    "10.0.101.0/24",
-    "10.0.102.0/24",
-    "10.0.103.0/24"
-  ]
-
-* enable_dns_hostnames = true
-  ena*le_dns_support   = true
-
-  tags = *
-    Terraform   = "true"
-    Envi*onment = "dev-test"
+  tags = {
+    Name = "learning-vpc"
   }
 }
 
-#######*##################################*######
+#################################################
+# Internet Gateway
+#################################################
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "learning-igw"
+  }
+}
+
+#################################################
+# Public Subnet
+#################################################
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+#################################################
+# Route Table
+#################################################
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+#################################################
 # Security Group
-##########*##################################*###
+#################################################
 
-module "blog_sg" {
- *source* = "terraform-aws-modules/security*group/aws"
-  version = "~> 5.0"
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Web Security Group"
+  vpc_id      = aws_vpc.main.id
 
- *name   = "blog-sg"
-  vpc_id = modu*e.blog_vpc.vpc_id
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  ingress_rules*= [
-    "http-80-tcp",
-    "https-443-tcp",
-    "ssh-tcp"
-  ]
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  ingr*ss_cidr_blocks = [
-    "0.0.0.0/0"
-  ]
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  egress_rules = [
-    "all-all"
-  ]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  egress_cidr_blocks = [
-    "0.0.0.0/0"
-  ]
+  tags = {
+    Name = "web-sg"
+  }
 }
 
-#############*##################################*
-# EC2
-###########################*#####################
+#################################################
+# EC2 Instance
+#################################################
 
-resource "a*s_instance" "blog" {
-  ami        *           = data.aws_ami.app_ami.*d
-  instance_type          = var.i*stance_type
-  subnet_id           *  = module.blog_vpc.public_subnets*0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+resource "aws_instance" "blog" {
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+  associate_public_ip_address = true
 
-  t*gs = {
-    Name = "LearningTerrafo*m"
+  tags = {
+    Name = "LearningTerraform"
   }
+}
+
+#################################################
+# Outputs
+#################################################
+
+output "instance_id" {
+  value = aws_instance.blog.id
+}
+
+output "public_ip" {
+  value = aws_instance.blog.public_ip
 }
